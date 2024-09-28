b47dba38903302794330c26a6c40b07b76edbc34
OlvaraCarl <olvarac@gmail.com>
2024-09-28 22:38:00 +0800

    Update Clock.java

--- src/Clock.java
+++ src/Clock.java
@@ -2,37 +2,37 @@ import java.util.Scanner;
 
 public class Clock {
 
-    // Method to convert seconds to minutes
+    // Procedure to convert the seconds to minutes
     public double SecondsToMinutes(double seconds) {
         // There are 60 seconds in a minute
         return seconds / 60;
     }
 
-    // Method to convert seconds to hours
+    // Procedure to convert the seconds to hours
     public double SecondsToHours(double seconds) {
         // There are 3600 seconds in an hour (60 * 60)
         return seconds / 3600;
     }
 
-    // Method to convert minutes to seconds
+    // Procedure to convert the minutes to seconds
     public double MinutesToSeconds(double minutes) {
         // There are 60 seconds in a minute
         return minutes * 60;
     }
 
-    // Method to convert minutes to hours
+    // Procedure to convert the minutes to hours
     public double MinutesToHours(double minutes) {
         // There are 60 minutes in an hour
         return minutes / 60;
     }
 
-    // Method to convert hours to seconds
+    // Procedure to convert the hours to seconds
     public double HoursToSeconds(double hours) {
         // There are 3600 seconds in an hour (60 * 60)
         return hours * 3600;
     }
 
-    // Method to convert hours to minutes
+    // Procedure to convert the hours to minutes
     public double HoursToMinutes(double hours) {
         // There are 60 minutes in an hour
         return hours * 60;
@@ -47,7 +47,7 @@ public class Clock {
         System.out.println("zun\nLaboratory Exercise");
 
         do {
-            // Display the conversion menu
+            // Display or print
             System.out.println("[1] Seconds to Minutes");
             System.out.println("[2] Seconds to Hours");
             System.out.println("[3] Minutes to Seconds");
@@ -55,18 +55,18 @@ public class Clock {
             System.out.println("[5] Hours to Seconds");
             System.out.println("[6] Hours to Minutes");
 
-            // Ask the user to choose a conversion
+            // Ask the user to choose a conversion they wish to execute
             System.out.print("Choose the Conversion: ");
             int choice = input.nextInt();
 
             // Variable to store the value entered by the user
             double value, result = 0;
 
-            // Ask the user for the value to convert
+            // Ask the user for the value to convert the time
             System.out.print("Enter the Value: ");
             value = input.nextDouble();
 
-            // Perform the conversion based on the user's choice
+            // Execute the conversion based on the user's choice
             switch (choice) {
                 case 1:
                     result = clock.SecondsToMinutes(value);
@@ -96,7 +96,7 @@ public class Clock {
                     System.out.println("Invalid choice!");
             }
 
-            // Ask if the user wants to perform another conversion
+            // Ask if the user wants to execute another conversion
             System.out.print("Do you want to convert time? (Y/N): ");
             continueConverting = input.next().charAt(0);
 
@@ -107,3 +107,4 @@ public class Clock {
         System.out.println("Thank you for using the program!");
     }
 }
+
